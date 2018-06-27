62f33e56f72bd1f18308a3c97a40fbdfb6bc7cc0
chenfuwei <120606552@qq.com>
2018-06-27 14:55:40 +0800

    surfaceview jni
    surface jni show bitmap

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,6 +4,10 @@
 #include "../include/Student.h"
 #include "../include/JniBase.h"
 #include <android/bitmap.h>
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
 
 static JavaVM *g_jvm;
 Student *student;
@@ -50,11 +54,56 @@ Java_com_picturejni_MainActivity_copyBitmap(JNIEnv *env, jobject object, jobject
 
     int32_t *int32Src = (int32_t*)src;
     int32_t *int32Des = (int32_t*)des;
-    memcpy(int32Des, int32Src, srcInfo.width*srcInfo.height);
+    memcpy(int32Des, int32Src, srcInfo.width*srcInfo.height * 4);
 
     int srcUnLockPixel = AndroidBitmap_unlockPixels(env, srcBitmap);
     int desUnLockPixel = AndroidBitmap_unlockPixels(env, desBitmap);
     JNI_LOGI(" srcUnLockPixel = %d desUnLockPixel = %d", srcUnLockPixel, desUnLockPixel);
+}
+
+extern "C" JNIEXPORT void
+JNICALL
+Java_com_picturejni_MainActivity_jniSurface(JNIEnv *env, jobject object, jobject surface, jobject srcBitmap)
+{
+    JNI_LOGI("jniSurface");
+    AndroidBitmapInfo srcInfo;
+    int srcInfoStatus = AndroidBitmap_getInfo(env, srcBitmap, &srcInfo);
+    if(srcInfoStatus != ANDROID_BITMAP_RESULT_SUCCESS)
+    {
+        JNI_LOGI("getInfo src error!");
+    }
+
+    JNI_LOGI("srcInfo info width = %d height = %d",srcInfo.width, srcInfo.height);
+
+    int32_t * srcBuf;
+    int srcLocStatus = AndroidBitmap_lockPixels(env, srcBitmap, (void**)(&srcBuf));
+
+    ANativeWindow* nativeWindow = ANativeWindow_fromSurface(env, surface);
+    ANativeWindow_acquire(nativeWindow);
+    ANativeWindow_setBuffersGeometry(nativeWindow, srcInfo.width, srcInfo.height, WINDOW_FORMAT_RGBA_8888);
+    ANativeWindow_Buffer window_buffer;
+    int windowLock = ANativeWindow_lock(nativeWindow, &window_buffer,NULL);
+    if(windowLock == 0) {
+        JNI_LOGI("nativewindow info width = %d height = %d format = %d stride = %d",
+                 window_buffer.width, window_buffer.height, window_buffer.format,
+                 window_buffer.stride);
+        int32_t *desbuffer = (int32_t *) window_buffer.bits;
+        if (window_buffer.stride == srcInfo.width) {
+         memcpy(desbuffer, srcBuf, srcInfo.width * srcInfo.height);
+        }else{
+            int offet = window_buffer.stride - srcInfo.width;
+            for(int i = 0; i < srcInfo.height; i++)
+            {
+                //memcpy拷贝的是字节数长度
+                memcpy(desbuffer + i * window_buffer.stride, srcBuf + i * srcInfo.width, srcInfo.width * 4);
+            }
+        }
+        ANativeWindow_unlockAndPost(nativeWindow);
+    }
+
+
+    ANativeWindow_release(nativeWindow);
+    AndroidBitmap_unlockPixels(env, srcBitmap);
 }
 
 
--- app/src/main/java/com/picturejni/MainActivity.kt
+++ app/src/main/java/com/picturejni/MainActivity.kt
@@ -4,10 +4,12 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Surface
+import android.view.SurfaceHolder
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
+    lateinit var srcBitmap: Bitmap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,10 +17,14 @@ class MainActivity : AppCompatActivity() {
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
 
-        var srcBitmap = BitmapFactory.decodeResource(resources, R.mipmap.hh)
+        srcBitmap = BitmapFactory.decodeResource(resources, R.mipmap.hh)
         var desBitmap = Bitmap.createBitmap(srcBitmap.width, srcBitmap.height, Bitmap.Config.ARGB_8888)
         copyBitmap(srcBitmap, desBitmap)
         ivBitmap.setImageBitmap(desBitmap)
+
+        surfaceView.layoutParams.width = srcBitmap.width
+        surfaceView.layoutParams.height = srcBitmap.height
+        surfaceView.holder.addCallback(this)
     }
 
     /**
@@ -26,7 +32,8 @@ class MainActivity : AppCompatActivity() {
      * which is packaged with this application.
      */
     external fun stringFromJNI(): String
-    external fun copyBitmap(srcBitmap: Bitmap, desBitmap: Bitmap): Unit
+    external fun copyBitmap(srcBitmap: Bitmap, desBitmap: Bitmap)
+    external fun jniSurface(surface: Surface, srcBitmap: Bitmap)
 
     companion object {
 
@@ -34,5 +41,16 @@ class MainActivity : AppCompatActivity() {
         init {
             System.loadLibrary("native-lib")
         }
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+        jniSurface(holder!!.surface, srcBitmap)
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder?) {
+
     }
 }
